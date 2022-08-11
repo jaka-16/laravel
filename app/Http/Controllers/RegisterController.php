47c37b05efbc34e114ddd5371285ba74250a97e4
jaka-16 <pratama.jaka16@gmail.com>
2022-08-11 09:54:54 +0700

    .

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -51,20 +51,20 @@ class RegisterController extends Controller
 
 
         if(!Authent::isNumber($num_ktp,16)){
-            Session::flash('error', "character length is limit exceded, character limit only 16 chars and only number");
-            
+            Session::flash('error', "character length is limit exceded in Nomor KTP field, character limit only 16 chars and only number");
+            return redirect('registerbiodata');
         }else if(!Authent::lengthChar($jk,1)){
-            Session::flash('error', "character length is limit exceded, character limit only 1 char and only char L or P");
-            
+            Session::flash('error', "character length is limit exceded in jenis kelamin field, character limit only 1 char and only char L or P");
+            return redirect('registerbiodata');
         }else if(!Authent::isNumber($num_hp,12)){
-            Session::flash('error', "character length is limit exceded, character limit only 12 chars and only number");
-           
+            Session::flash('error', "character length is limit exceded in nomor hp field, character limit only 12 chars and only number");
+            return redirect('registerbiodata');
         }else if(!Authent::isNumber($num_hp_fr,12)){
-            Session::flash('error', "character length is limit exceded, character limit only 12 chars and only number");
-            
+            Session::flash('error', "character length is limit exceded in nomor hp referensi field, character limit only 12 chars and only number");
+            return redirect('registerbiodata');
         }else if(!Authent::lengthChar($gol_darah,2)){
-            Session::flash('error', "character length is limit exceded, character limit only 2 chars and only A, AB, O, B");
-            
+            Session::flash('error', "character length is limit exceded in golongan darah field, character limit only 2 chars and only A, AB, O, B");
+            return redirect('registerbiodata');
         }
         
         
@@ -93,7 +93,46 @@ class RegisterController extends Controller
 
     public function update(request $request){
         $id = $request->id;
+        $posisi = $request->position_int;
+        $nama = $request->name;
+        $num_ktp = $request->num_id;
+        $tempat_lhr = $request->placeofbirth;
+        $birthdate = $request->birthdate;
+        $date = date_create($birthdate);
+        if(!$date && strlen($birthdate) != 8){
+            Session::flash('error', "incorrect format date, use format YYYYMMDD"); 
+        }
+        $tgl_lhr = date_format($date, "Y-m-d"); 
+        $jk = $request->gender;
+        $agama = $request->religion;
+        $gol_darah = $request->blood_class;
+        $status = $request->status;
+        $alamat_ktp = $request->address_id;
+        $almat_tinggal = $request->address;
+        $email = $request->email;
+        $num_hp = $request->num_hp;
+        $num_hp_fr = $request->num_hp_fr;
         
+       
+
+        if(!Authent::isNumber($num_ktp,16)){
+            Session::flash('error', "character length is limit exceded in Nomor KTP field, character limit only 16 chars and only number");
+            return redirect('registerbiodata');
+        }else if(!Authent::lengthChar($jk,1)){
+            Session::flash('error', "character length is limit exceded in jenis kelamin field, character limit only 1 char and only char L or P");
+            return redirect('registerbiodata');
+        }else if(!Authent::isNumber($num_hp,12)){
+            Session::flash('error', "character length is limit exceded in nomor hp field, character limit only 12 chars and only number");
+            return redirect('registerbiodata');
+        }else if(!Authent::isNumber($num_hp_fr,12)){
+            Session::flash('error', "character length is limit exceded in nomor hp referensi field, character limit only 12 chars and only number");
+            return redirect('registerbiodata');
+        }else if(!Authent::lengthChar($gol_darah,2)){
+            Session::flash('error', "character length is limit exceded in golongan darah field, character limit only 2 chars and only A, AB, O, B");
+            return redirect('registerbiodata');
+        }
+
+
         $datas = [
 
             "position_int" => $request->position_int, 
